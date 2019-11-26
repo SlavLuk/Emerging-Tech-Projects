7fd2f9d4ef2ee66e6b711774766a5a7bdb15b6ef
g00339839 <g00339839@gmit.ie>
2019-11-26 01:13:22 +0000

    got image processed

--- webapp/main.py
+++ webapp/main.py
@@ -2,7 +2,13 @@ import flask as fl
 import base64
 import re
 import cv2
-from keras.models import load_model
+from tensorflow.keras.models import load_model
+import numpy as np
+import matplotlib.pyplot as plt
+from imageio import imread
+import io
+import PIL.Image as pyimage
+
 
 
 app = fl.Flask(__name__)
@@ -17,18 +23,40 @@ def home():
 @app.route("/predict", methods=['GET', 'POST'])
 def predict():
 
-    imgStr = fl.request.values.get("image")
+    #reads from ajax sent request
+    imgStr = fl.request.values.get("image") 
+    # split a string on comma delimiter and get second element
+    subImgStr = imgStr.split(',')[1]
+    # decode string into 
+    imageBytes = base64.b64decode(subImgStr)
+    # convert bytes into python image 
+    pyimg = pyimage.open(io.BytesIO(imageBytes))
+    # convert python image into numpy array
+    imgArr = np.asarray(pyimg)
+    
+    # resize image to 28 x 28 dimension
+    imgResize = cv2.resize(imgArr, (28,28), interpolation = cv2.INTER_AREA)
+    # convert to gray color with one channel
+    imgGray = cv2.cvtColor(imgResize, cv2.COLOR_BGR2GRAY)
+    # scale for fast processing
+    imgGray= imgGray/255
+  
+    # reshape to fit cnn model 
+    img = imgGray.reshape(-1,28,28,1).astype('float32')
+ 
+    # load saved trained model
+    model = load_model('../model/model.h5')
+    # predict classes
+    result = model.predict_classes(img)
 
-    imgStrSub = re.sub('^data:image/.+;base64,', '', imgStr)
-    image = base64.b64decode(imgStrSub)
-    img = cv2.resize(image, (28, 28), interpolation=cv2.INTER_AREA)
-    arr = img.reshape(-1, 28, 28, 1).astype('float32')
-    arr /= 255
+    
+    
+    print(result)
 
-    model = load_model('my_model.h5')
-    model.predict(arr)
+    return fl.make_response(str(result[0]))
 
-    return fl.Response(imgStr)
+
+
 
 
 if __name__ == "__main__":
--- webapp/static/js/datahandler.js
+++ webapp/static/js/datahandler.js
@@ -1,18 +1,15 @@
 $(function() {
   var canvas = document.getElementById("canvas");
-  context = canvas.getContext("2d");
-  $("#predict-button").click(async function(e) {
+
+  $("#predict-button").click(function(e) {
     e.preventDefault();
 
     //get image data from canvas
     var imageData = canvas.toDataURL();
 
     $.post("/predict", { image: imageData }, function(data) {
-      console.log(data);
       $("#predict").text(data);
     });
-
-    //$("#chart_box").attr("src", imageData);
   });
 
   $("#clear-button").click(async function() {
